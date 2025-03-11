6072515417b4a4780c5cb65e117dec2439fcb92e
Jasper Nykänen <jasperjnykanen@gmail.com>
2025-03-11 16:32:22 +0200

    top level await

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import wasm from 'vite-plugin-wasm'
 import tailwindcss from '@tailwindcss/vite'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [wasm(), react(), tailwindcss()]
+  plugins: [wasm(), react(), tailwindcss(), topLevelAwait()]
 })
